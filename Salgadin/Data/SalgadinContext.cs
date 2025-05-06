0ac7804469c057a8739039127960f99874ec0c93
wemer <wemersoncontas22@gmail.com>
2025-05-06 19:10:59 -0300

    feat(data): add SalgadinContext with Expenses DbSet

---
+++ Salgadin/Data/SalgadinContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Salgadin.Models;
+
+namespace Salgadin.Data
+{
+    public class SalgadinContext : DbContext
+    {
+        public SalgadinContext(DbContextOptions<SalgadinContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; } = null!;
+    }
+}
