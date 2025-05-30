575542052255d4f2476bce129b9d9a5350f0e478
wemer <wemersoncontas22@gmail.com>
2025-05-30 12:35:17 -0300

    feat(auth): add DTOs for user registration and login

---
+++ Salgadin/DTOs/UserRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Salgadin.DTOs
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
